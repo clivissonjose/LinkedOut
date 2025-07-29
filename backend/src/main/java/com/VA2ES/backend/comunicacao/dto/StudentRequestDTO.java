2a5f63fcbb82925655e86b193fdc9b38ac1e053b
GENILDOBURGOS <gnldburgos@gmail.com>
2025-07-29 00:52:21 -0300

    feat(dto): adiciona DTO de requisição para cadastro e atualização de estudante

---
+++ backend/src/main/java/com/VA2ES/backend/comunicacao/dto/StudentRequestDTO.java
@@ -0,0 +1,14 @@
+package com.VA2ES.backend.comunicacao.dto;
+
+import java.time.LocalDate;
+
+public class StudentRequestDTO {
+    public String fullName;
+    public LocalDate birthDate;
+    public String cpf;
+    public String phone;
+    public String course;
+    public String currentPeriod;
+    public String academicSummary;
+    public Long userId;
+}
