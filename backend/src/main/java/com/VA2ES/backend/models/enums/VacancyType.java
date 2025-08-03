cc73e86ed62230c9fa47a53a0202bb3c6a306c84
Vitorass0 <vitor.ass@hotmail.com>
2025-08-02 22:33:14 -0300

    Feat: Implementando a entidade referente a vaga.

---
+++ backend/src/main/java/com/VA2ES/backend/models/enums/VacancyType.java
@@ -0,0 +1,7 @@
+package com.VA2ES.backend.models.enums;
+
+public enum VacancyType {
+    ESTÁGIO,
+    TRAINEE,
+    EMPREGO;
+}
---
+++ backend/src/main/java/com/VA2ES/backend/repositories/VacancyRepository.java
@@ -0,0 +1,8 @@
+package com.VA2ES.backend.repositories;
+
+import com.VA2ES.backend.models.Vacancy;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface VacancyRepository extends JpaRepository<Vacancy, Long> {
+}
