4ab648b0f5b3fd4801e6d41e94a184100f4c865d
GENILDOBURGOS <gnldburgos@gmail.com>
2025-09-08 22:08:54 -0300

    fix(student): corrigindo erro CI/CD do github

--- backend/src/main/java/com/va2es/backend/repositories/ApplicationRepository.java
+++ backend/src/main/java/com/va2es/backend/repositories/ApplicationRepository.java
@@ -9,4 +9,6 @@ import java.util.Optional;
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
     Optional<Application> findByStudentIdAndVacancyId(Long studentId, Long vacancyId);
     List<Application> findByVacancy_Company_Id(Long companyId);
+
+    List<Application> findByStudentId(Long studentId);
 }
